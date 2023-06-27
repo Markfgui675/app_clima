e6d7dadcca1487fe33771762c9c5d9ee0e6ace9a
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-06-27 17:25:05 -0300

    Styles

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -12,6 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   ValueNotifier<Color> color = ValueNotifier(Colors.black);
+  ValueNotifier<dynamic> lottieFile = ValueNotifier(Lottie.asset('assets/rain.json'));
 
   _pesquisar() async {
     String chave_api = '18ebbd17';
@@ -32,9 +36,14 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: Colors.deepOrangeAccent
+            ),
+            elevation: 0,
             backgroundColor: MaterialStateColor.resolveWith((Set<MaterialState> states){
               if(states.contains(MaterialState.scrolledUnder)){
                 color.value = Colors.white;
@@ -43,27 +52,68 @@ class _HomeState extends State<Home> {
               color.value = Colors.black;
               return Colors.deepOrangeAccent;
             }),
-            toolbarHeight: 120,
-            expandedHeight: 200,
+            toolbarHeight: 320,
+            expandedHeight: 420,
             pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              title: AnimatedBuilder(
-                animation: color,
-                builder: (context, _){
-                  return Text('Cliente', style: TextStyle(color: color.value),);
-                },
+            flexibleSpace: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 24, top: 24),
+                        child: Icon(Icons.menu),
+                      )
+                  ),
+                  Expanded(
+                      flex: 3,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 24),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('25°', style: TextStyle(fontSize: 100),),
+                                  SizedBox(height: 5,),
+                                  Text('Limpo', style: TextStyle(fontSize: 20),),
+                                  SizedBox(height: 5,),
+                                  Row(
+                                    children: [
+                                      Text('Brasília'),
+                                      Icon(Icons.pin_drop)
+                                    ],
+                                  ),
+                                  SizedBox(height: 5,),
+                                  Text('Info', style: TextStyle(fontSize: 20),),
+
+                                ],
+                              ),
+                            )
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: Lottie.asset('assets/rain.json'),
+                            )
+                          ),
+                        ],
+                      )
+                  ),
+                ],
               ),
-
             ),
-
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
-              childCount: 9,
+              childCount: 1,
               (context, int index){
                 return Container(
-                  height: 120,
-                  color: Colors.red,
+                  height: 680,
+                  color: Colors.transparent,
                 );
               }
             ),
