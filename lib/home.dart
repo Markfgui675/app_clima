d04135422d188064a42b0f8da5254c928bec74c7
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-07-04 16:40:41 -0300

    Restart layout

--- lib/home.dart
+++ lib/home.dart
@@ -14,9 +14,6 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  ValueNotifier<Color> color = ValueNotifier(Colors.black);
-  ValueNotifier<dynamic> lottieFile = ValueNotifier(Lottie.asset('assets/rain.json'));
-
   _pesquisar() async {
     String chave_api = '18ebbd17';
 
@@ -36,91 +33,13 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            systemOverlayStyle: SystemUiOverlayStyle(
-              statusBarColor: Colors.deepOrangeAccent
-            ),
-            elevation: 0,
-            backgroundColor: MaterialStateColor.resolveWith((Set<MaterialState> states){
-              if(states.contains(MaterialState.scrolledUnder)){
-                color.value = Colors.white;
-                return Colors.deepPurple;
-              }
-              color.value = Colors.black;
-              return Colors.deepOrangeAccent;
-            }),
-            toolbarHeight: 320,
-            expandedHeight: 420,
-            pinned: true,
-            flexibleSpace: SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 24, top: 24),
-                        child: Icon(Icons.menu),
-                      )
-                  ),
-                  Expanded(
-                      flex: 3,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 24),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text('25°', style: TextStyle(fontSize: 100),),
-                                  SizedBox(height: 5,),
-                                  Text('Limpo', style: TextStyle(fontSize: 20),),
-                                  SizedBox(height: 5,),
-                                  Row(
-                                    children: [
-                                      Text('Brasília'),
-                                      Icon(Icons.pin_drop)
-                                    ],
-                                  ),
-                                  SizedBox(height: 5,),
-                                  Text('Info', style: TextStyle(fontSize: 20),),
-
-                                ],
-                              ),
-                            )
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Center(
-                              child: Lottie.asset('assets/rain.json'),
-                            )
-                          ),
-                        ],
-                      )
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              childCount: 1,
-              (context, int index){
-                return Container(
-                  height: 680,
-                  color: Colors.transparent,
-                );
-              }
-            ),
-
-          )
-        ],
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.blueAccent
+        ),
+        toolbarHeight: 420,
       ),
+      body: Container(),
     );
   }
 }
